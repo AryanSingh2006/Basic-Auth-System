6e950c6cc07a78771c727f6d75bc31839fd0ca71
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-11-13 12:09:20 +0530

    .

--- src/main/java/com/example/Basic_Auth_System/Service/UserService.java
+++ src/main/java/com/example/Basic_Auth_System/Service/UserService.java
@@ -3,10 +3,10 @@ package com.example.Basic_Auth_System.Service;
 import com.example.Basic_Auth_System.Dto.UserResponse;
 import com.example.Basic_Auth_System.Model.User;
 import com.example.Basic_Auth_System.Model.UserPrincipal;
-import org.hibernate.annotations.SecondaryRow;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class UserService {
